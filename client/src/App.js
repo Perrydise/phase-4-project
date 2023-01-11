fe1302d00c0fcfa51e49f2aea5ba3bb71a7b7da4
Perry Jorss <pjorss123@gmail.com>
2023-01-11 13:30:05 -0500

    added a handle submit to login

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import Auth from './components/Auth'
+// import Auth from './components/Auth'
 import './App.css';
 import { useState, useEffect } from 'react';
 import LogIn from './LogIn';
--- client/src/LogIn.js
+++ client/src/LogIn.js
@@ -1,51 +1,49 @@
-import React from 'react';
+import React, {useState} from "react";
 
-function LogIn ({ onLogin }) {
+function LogIn () {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConfirmation, setPasswordConfirmation] = useState("");
   
     function handleSubmit(e) {
       e.preventDefault();
-      fetch("/signup", {
+      const user = {
+        username,
+        password
+      }
+      fetch('/login',{
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username,
-          password,
-          password_confirmation: passwordConfirmation,
-        }),
+        headers:{'Content-type':'application/json'},
+        body:JSON.stringify(user)
       })
-        .then((r) => r.json())
-        .then(onLogin);
+      .then((r) => r.json())
+      .then((user) => onLogin(user))
     }
   
     return (
       <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username:</label>
+        <label htmlFor="username">Username: </label>
         <input
           type="text"
           id="username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-        <label htmlFor="password">Password:</label>
+        <label htmlFor="password">Password: </label>
         <input
           type="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <label htmlFor="password_confirmation">Confirm Password:</label>
+        <label htmlFor="password_confirmation">Confirm Password: </label>
         <input
           type="password"
           id="password_confirmation"
           value={passwordConfirmation}
           onChange={(e) => setPasswordConfirmation(e.target.value)}
         />
-        <button type="submit">Submit</button>
+        <button type="submit">Sign up</button>
       </form>
     );
 }
